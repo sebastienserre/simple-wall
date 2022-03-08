f1b2d55db65f10c2b98a01d24f6a89043c8ad76c
Sébastien SERRE <sebastien@serree.com>
2022-03-08 21:55:43 +0100

    add plugin version in html markup for easily support

--- simple-wall.php
+++ simple-wall.php
@@ -20,6 +20,7 @@ namespace SimpleWall;
 
 add_action( 'plugins_loaded', 'SimpleWall\define_constant' );
 function define_constant() {
+	define( 'SIMPLE_VERSION', '1.1.0' );
 	define( 'SIMPLE_FB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 	define( 'SIMPLE_FB_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 	define( 'SIMPLE_FB_PLUGIN_DIR', untrailingslashit( SIMPLE_FB_PLUGIN_PATH ) );
