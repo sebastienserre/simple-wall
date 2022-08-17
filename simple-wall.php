73a00fcb590be6fff7fb413c14ca1a8495b10e57
Sébastien SERRE <sebastien@thivinfo.com>
2022-08-17 09:43:11 +0200

    bump to 1.1.3

--- simple-wall.php
+++ simple-wall.php
@@ -10,7 +10,7 @@ namespace SimpleWall;
  * Text Domain: simple-wall
  * Requires at least: 4.6
  * Tested up to: 6.1
- * Version: 1.1.2
+ * Version: 1.1.3
  * License: GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  **/
@@ -21,7 +21,7 @@ namespace SimpleWall;
 
 add_action( 'plugins_loaded', 'SimpleWall\define_constant' );
 function define_constant() {
-	define( 'SIMPLE_VERSION', '1.1.2' );
+	define( 'SIMPLE_VERSION', '1.1.3' );
 	define( 'SIMPLE_FB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 	define( 'SIMPLE_FB_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 	define( 'SIMPLE_FB_PLUGIN_DIR', untrailingslashit( SIMPLE_FB_PLUGIN_PATH ) );
